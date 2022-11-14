00a600d5ad164984c1941cb7978c2550cca48369
Hashir Baig <hashirbaig456@gmail.com>
2022-11-14 15:45:29 +0500

    bug fix

--- utils/common.ts
+++ utils/common.ts
@@ -1,11 +1,11 @@
 import { format } from "date-fns"
 
-export const capitalize = (s: string) => {
+export const capitalize = (s: any) => {
   if (typeof s !== "string") return ""
   return s.charAt(0).toUpperCase() + s.slice(1)
 }
 
-export const getFormattedDate = (date: string) => {
+export const getFormattedDate = (date: any) => {
   if (typeof date !== "string") return date
 
   return format(new Date(date), "dd MMMM yy")
